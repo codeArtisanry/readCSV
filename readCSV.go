48515c9b3997e5724b877b06c0ee98a0b7e9fb6d
Vatsal <chauhanvatsal55@gmail.com>
2022-07-24 09:28:13 +0530

    feat: add csv reader

---
+++ readCSV.go
@@ -0,0 +1,23 @@
+package readCsv
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+// ReadCsv accepts a file and returns its content as a multi-dimentional type
+// with lines and each column. Only parses to string type.
+func ReadCsv(filename string) ([][]string, error) {
+	// Open CSV file
+	f, err := os.Open(filename)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+	// Read File into a Variable
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+	return lines, nil
+}
